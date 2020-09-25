925eb39489595c7102943e721302695812f42fdf
Selfay <chris.lee90@hotmail.co.uk>
2020-09-25 23:35:59 +0100

    Updates

---
+++ precache-manifest.124007d7be5832bc80c1db22953d3871.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c607210b797c2739b756e315c032008",
+    "url": "/holiday-planner-client/index.html"
+  },
+  {
+    "revision": "2f45c26ef8bee1bbeb6f",
+    "url": "/holiday-planner-client/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "502c5e8cea36b18c0669",
+    "url": "/holiday-planner-client/static/css/main.a2eaec7a.chunk.css"
+  },
+  {
+    "revision": "2f45c26ef8bee1bbeb6f",
+    "url": "/holiday-planner-client/static/js/2.39e07f6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/holiday-planner-client/static/js/2.39e07f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502c5e8cea36b18c0669",
+    "url": "/holiday-planner-client/static/js/main.e6e33b7a.chunk.js"
+  },
+  {
+    "revision": "9297101a02cba24aa9f9",
+    "url": "/holiday-planner-client/static/js/runtime-main.75aeda02.js"
+  },
+  {
+    "revision": "5f5c1e117acb015e5fd1050e004a569a",
+    "url": "/holiday-planner-client/static/media/a.5f5c1e11.png"
+  },
+  {
+    "revision": "fea06681b2c72e13d357318f460ecc65",
+    "url": "/holiday-planner-client/static/media/mountains.fea06681.jpg"
+  },
+  {
+    "revision": "9bb365ddca9d7942b9091af688e3918f",
+    "url": "/holiday-planner-client/static/media/palmTree.9bb365dd.jpg"
+  },
+  {
+    "revision": "58c1dbd25bf00b9b1bb75f4d7adaee2b",
+    "url": "/holiday-planner-client/static/media/seaside1.58c1dbd2.jpg"
+  }
+]);
